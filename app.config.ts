9060368de92adf31c11244b94def3bf2c83d21fb
nora2605 <nora.ja2605@gmail.com>
2025-10-30 22:24:14 +0100

    solid init

---
+++ app.config.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from "@solidjs/start/config";
+import tailwindcss from "@tailwindcss/vite"
+
+export default defineConfig({
+  vite: {
+    ssr: { external: ["drizzle-orm"] },
+    plugins: [tailwindcss()]
+  },
+});
